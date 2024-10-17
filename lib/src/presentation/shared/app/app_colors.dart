fd9554b79569551717acecd9a96ea6fba12d8895
MateusLucasDaSilva <mateuslukas505@gmail.com>
2024-10-17 20:25:57 -0300

    new color

--- lib/src/presentation/shared/app/app_colors.dart
+++ lib/src/presentation/shared/app/app_colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color ligth = Color(0xffd7d7d7);
+  static const Color light = Color(0xffd7d7d7);
+  static const Color back = Color(0xff1a1a1a);
 }
